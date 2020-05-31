7eaa651a58e52ac7991bba6e65d42d03757872b4
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2020-05-31 15:47:13 +0200

    Implement Wallet, Operation and Diagram models
    Wallet's main responsibility is to keep the amount of money that user
    keeps. Each time the Income or Outcome is made on the Wallet the
    notification is made to the Diagram to register that new operation has
    been made.
    Diagram's main responsibility is to store all the operations made on the
    Wallet, so they can be published to the View. Diagram reacts on the
    Wallet notification with the Update() method and adds new operation with
    the latest amount of it.
    Operation class represents single operation that is made on the Wallet.
    It implements the Prototype design pattern so it can be easily created
    based on the latest operation and it's data.

---
+++ App/Models/Operation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace App.Models
+{
+    public interface IOperationPrototype
+    {
+        public Operation Clone();
+        public Operation Clone( decimal amount );
+    }
+
+
+    public class Operation : IOperationPrototype
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+
+        public DateTime Time
+        {
+            get;
+            set;
+        }
+
+
+        public decimal Amount
+        {
+            get;
+            set;
+        }
+
+
+        public Operation Clone()
+        {
+            var clone = this.MemberwiseClone() as Operation;
+            clone.ID++;
+            clone.Time = DateTime.Now;
+
+            return clone;
+        }
+
+
+        public Operation Clone( decimal amount )
+        {
+            var clone = Clone();
+            clone.Amount = amount;
+
+            return clone;
+        }
+    }
+}
---
+++ App/Models/Wallet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace App.Models
+{
+    public interface ITopic
+    {
+        public void Attach( IObserver observer );
+        public void Detach( IObserver observer );
+
+        public void Notify( decimal amount );
+    }
+
+
+    public class Wallet : ITopic
+    {
+        public void Income( decimal amount ) => Money += amount;
+
+        public void Outcome( decimal amount ) => Money -= amount;
+
+
+        public decimal Money
+        {
+            get => money;
+            private set
+            {
+                Notify( Math.Abs( money - value ) );
+                money = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return money.ToString();
+        }
+
+
+        public void Attach( IObserver observer ) => observers.Add( observer );
+
+        public void Detach( IObserver observer ) => observers.Remove( observer );
+
+
+        public void Notify( decimal amount )
+        {
+            foreach( var observer in observers )
+            {
+                observer.Update( this, amount );
+            }
+        }
+
+
+        private decimal money;
+
+        private List<IObserver> observers = new List<IObserver>();
+    }
+}
